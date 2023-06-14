b0cf73c97ecf76dab5d18f31b2bfa8adf8090d46
Saraqawasmeh <saraqawasmeh90@gamil.com>
2023-06-14 16:52:12 +0300

    update

---
+++ cypress/integ/ex/locked_out.js
@@ -0,0 +1,8 @@
+describe('this is s standered test',()=>{
+    it('is the test',()=>{
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"]').type('locked_out_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+    })
+})
